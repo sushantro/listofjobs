1640b165eb8951489c21474fb7507d5c42802106
sushantro <62639871+sushantro@users.noreply.github.com>
2021-06-13 13:41:15 +0530

    Update Prt.jsx

--- src/Prt.jsx
+++ src/Prt.jsx
@@ -4,6 +4,11 @@ import img1 from "./h.jpeg"
 import About1 from "./About1"
 import Contact from "./Contact"
 import AppFooter from "./header";
+import Slide from "./slide";
+
+
+
+import { Link } from 'react-router-dom';
 
 const Prt = () => {
     return (
@@ -12,7 +17,7 @@ const Prt = () => {
                 <div className="container-fluid nav_bg">
                     <div className="row">
                         <div className="col-md-6 pt-5 pt-lg-10 order-2 order-lg-1 d-flex justify-content-center flex-column">
-                            <h1>
+                            <h1 className="text">
                                 Grow ur business with <strong className="brand-name">DHANDA PANI </strong>
 
                             </h1>
@@ -26,8 +31,11 @@ const Prt = () => {
 
                             <div class="row">
                             <div class="col text-center">
-                            <a href="" className="btn-get-started">WHAT IS UR NEED</a>
-                            <a href="" className="btn-get-started">Search for job</a>
+                            <Link to="/contact" className="btn-get-started">WHAT IS UR NEED</Link>
+                            <Link to="/about" className="btn-get-started">Search for job</Link>
+                             {/* <Redirect to="/contact" className="btn-get-started">WHAT IS UR NEED</Redirect>
+                            <Redirect to="" className="btn-get-started">Search for job</Redirect> */}
+
 
                             </div>
                             </div>
@@ -49,7 +57,9 @@ const Prt = () => {
 
             <About1/>
             <Contact/>
+            <Slide/>
             <AppFooter/>
+            
 
 
 
