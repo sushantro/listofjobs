ca2e003c40435b9891176b0dff7608727302dd2e
sushantro <62639871+sushantro@users.noreply.github.com>
2021-06-17 14:36:49 +0530

    Update Navigation.jsx

--- src/Navigation.jsx
+++ src/Navigation.jsx
@@ -1,25 +1,29 @@
 import React from 'react'
-import AppBar from '@material-ui/core/AppBar';
+// import AppBar from '@material-ui/core/AppBar';
 import {  NavLink } from 'react-router-dom';
 import "./Dofa.css"
 import "bootstrap/dist/css/bootstrap.css";
-import { red } from '@material-ui/core/colors';
+// import logo from "./logo1.png"
+// import { red } from '@material-ui/core/colors';
 
 
 
 const Navigation = () => {
+  
   return (
    
   
-    <div className="container-fluid nav-bg">
+    <div className="container-fluid navbar-light animate-navbar">
       <div className="row">
         <div className="col-10 mx-auto">
 
      
 
-    <nav className="navbar navbar-expand-sm fixed-top">
+    <nav className="navbar navbar-expand-sm fixed-top bg-white shadow">
 
-      <a className="navbar-brand">DHANDA PANI</a>
+      {/* <a className="navbar-brand">DHANDA PANI</a> */}
+      <b className="bold">DHANDA PANI</b>
+      {/* <img className="navbar-brand" src={logo} alt="k"/> */}
 
       <ul className="navbar-nav ml-auto">
         <li className="nav-item active">
@@ -36,21 +40,21 @@ const Navigation = () => {
           
         </li>
         <li className="nav-item">
-          <NavLink className="nav-link" to="/login1">Login</NavLink>
+          <NavLink className="nav-link" to="/login3">Login</NavLink>
           
         </li>
         <li className="nav-item">
           <NavLink className="nav-link" to="/help">Help</NavLink>
           
         </li>
-        <li className="nav-item">
+        {/* <li className="nav-item">
           <NavLink className="nav-link" to="/browse" style={{backgroundColor:"red",fontFamily:"sans-serif"}}>Browse Jobs</NavLink>
           
-        </li>
-        <li className="nav-item">
+        </li> */}
+        {/* <li className="nav-item">
           <NavLink className="nav-link" to="/add">cart</NavLink>
           
-        </li>
+        </li> */}
 
       </ul>
     </nav>
