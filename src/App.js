b09092dad9dd73d0e33deb5c25752ef3e2fc41a9
sushantro <62639871+sushantro@users.noreply.github.com>
2021-06-17 14:41:11 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,6 +9,17 @@ import Navigation from './Navigation';
 import FormDialog from "./Modal";
 import Login1 from "./Login1"
 import Accordion from "./Accordion"
+
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+// import  About  from "./About"
+import Contact from "./Contact";
+import Error from './Error';
+import {BrowserRouter,Switch,Route, Redirect} from "react-router-dom";
+import Navigation from './Navigation';
+// import Home from './Home';
+import FormDialog from "./Modal";
+import Login from "./Login3"
+import Accordion from "./Accordion"
 import Prt from "./Prt"
 import  About1  from "./About1"
 import Cards from './cards';
@@ -31,7 +42,7 @@ function App() {
      <Route exact path="/" component={Prt}/>
       <Route exact path="/About" component={About1}/>
       <Route exact  path="/card" component={Cards}/>
-      <Route exact  path="/login1" component={Login1}/>
+      <Route exact  path="/login3" component={Login}/>
       <Route exact  path="/contact" component={Contact}/>
       <Route exact  path="/help" component={Accordion}/>
      
